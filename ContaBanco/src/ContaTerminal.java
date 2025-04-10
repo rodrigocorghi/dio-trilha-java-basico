835d089e14188b5f90b6b4ce706f0d736da656a7
rodrigocorghi <rodrigocorghi@yahoo.com.br>
2025-04-10 14:15:45 -0300

    Conta Terminal Pronto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // TODO: Conhecer e importar a classe scanner
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+
+            // Exibir as mensagens para o usuário
+            // Obter pela classe Scanner os valores no terminal
+
+            System.out.println("Digite sua Agência:");
+            String $agencia = scanner.nextLine();
+
+            System.out.println("Digite o número da conta:");
+            int $numeroConta = scanner.nextInt();
+
+            System.out.println("Digite o seu nome:");
+            String $nome = scanner.next();
+
+            System.out.println("Digite o seu saldo:");
+            double $saldo = scanner.nextDouble();
+
+            // Exibir a mensagem com os dados da conta
+            System.out.println("Olá " + $nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + $agencia + ", conta " + $numeroConta + " e seu saldo " + $saldo + " já está disponível para saque.");
+        }
+
     }
 }
